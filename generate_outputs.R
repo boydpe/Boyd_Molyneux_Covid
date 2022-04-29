a2748462cb7c01a8aadf967839244669c240b08e
YOUR FULL NAME <molyneuj@oregonstate.edu>
2022-04-28 17:24:23 -0700

    Update source file for step 04

--- generate_outputs.R
+++ generate_outputs.R
@@ -5,11 +5,11 @@ source("Code/01-create-production-data.R")
 source("Code/02-read-production-data.R")
 source("Code/03-misd-functions.R")
 source("Code/03-simulation-function.R")
+source("Code/04-estimate-models.R")
+source("Code/04-generate-simulations.R")
 
 # Steps that need cleaning
 
-# source("Code/04-estimate-models.R")
-# source("Code/05-generate-simulations.R")
 # source("Code/06-model-estimates-to-dataframe.R")
 # source("Code/06-generate-plots.R")
 # source("Code/06-generate-summary-tables.R")
