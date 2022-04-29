0b4deadd8a1a6a2cdc9862aa95d85626c19f3706
YOUR FULL NAME <molyneuj@oregonstate.edu>
2022-04-28 17:43:48 -0700

    Update source file to generate plots in step 06

--- generate_outputs.R
+++ generate_outputs.R
@@ -8,8 +8,8 @@ source("Code/03-simulation-function.R")
 source("Code/04-estimate-models.R")
 source("Code/04-generate-simulations.R")
 source("Code/05-model-estimates-to-dataframe.R")
+source("Code/06-generate-plots.R")
 
 # Steps that need cleaning
 
-# source("Code/06-generate-plots.R")
 # source("Code/06-generate-summary-tables.R")
