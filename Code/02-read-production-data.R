565c14192c1e691ceffff7f815f49a3b142688a3
YOUR FULL NAME <molyneuj@oregonstate.edu>
2022-04-28 17:06:46 -0700

    Fix file paths is step 02-read-production-data

--- Code/02-read-production-data.R
+++ Code/02-read-production-data.R
@@ -1,4 +1,4 @@
 # Read in production data
-state_sub_rally = readRDS("Data/state_data.Rds")
-all_data = readRDS("Data/county_data.Rds")
-rally = readRDS("Data/rally_edit.Rds")
+state_sub_rally = readRDS("Data/Processed/state_data.Rds")
+all_data = readRDS("Data/Processed/county_data.Rds")
+rally = readRDS("Data/Processed/rally_edit.Rds")
