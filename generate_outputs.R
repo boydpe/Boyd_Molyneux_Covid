56961a66f482d7038a8725f234a3ebf729846a53
YOUR FULL NAME <molyneuj@oregonstate.edu>
2022-04-28 17:28:03 -0700

    Source file now up to date through step 05

--- generate_outputs.R
+++ generate_outputs.R
@@ -7,9 +7,9 @@ source("Code/03-misd-functions.R")
 source("Code/03-simulation-function.R")
 source("Code/04-estimate-models.R")
 source("Code/04-generate-simulations.R")
+source("Code/05-model-estimates-to-dataframe.R")
 
 # Steps that need cleaning
 
-# source("Code/06-model-estimates-to-dataframe.R")
 # source("Code/06-generate-plots.R")
 # source("Code/06-generate-summary-tables.R")
